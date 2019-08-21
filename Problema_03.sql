ad62eb47d69baba26c5e66dd1500605674c97a51
EliezerMisaelGomez10 <54338611+EliezerMisaelGomez10@users.noreply.github.com>
2019-08-21 20:06:36 +0200

    Create Problema_03.sql

---
+++ Problema_03.sql
@@ -0,0 +1,31 @@
+
+-- Problema 03
+create database BDI;
+
+show databases;
+
+use  BDI;
+
+ drop table if exists agenda;
+
+create table BDI.agenda(
+	nombre varchar(20),
+	domicilio varchar(30),
+	telefono varchar(11)
+) ;
+
+create table BDI.agenda(
+	nombre varchar(20),
+	domicilio varchar(30),
+	telefono varchar(11)
+) ;
+
+ 
+ show tables;
+ 
+describe agenda;
+
+drop table if exists agenda;
+
+ drop table agenda;
+
